f7b05a05f4280ffe2c6580e2a115c2400c84603b
Thongchai Kitiyanantawong <mypumz@gmail.com>
2017-02-28 02:14:50 +0700

    Refetching a query when delete song

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -8,6 +8,7 @@ import query from '../queries/fetchSongs'
 class SongList extends Component {
   onDeleteSong (id) {
     this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch())
   }
 
   renderSongs () {
